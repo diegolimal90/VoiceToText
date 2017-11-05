84e6d6430668889dd052e9bc765bec788835a439
diegolimal90 <diego.lima.lopes@hotmail.com.br>
2017-11-05 06:53:44 -0200

    Enviando aplicação basica

---
+++ VoiceToText/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace VoiceToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.PBarAudio = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(493, 359);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // PBarAudio
+            // 
+            this.PBarAudio.Location = new System.Drawing.Point(12, 377);
+            this.PBarAudio.Name = "PBarAudio";
+            this.PBarAudio.Size = new System.Drawing.Size(493, 36);
+            this.PBarAudio.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(517, 425);
+            this.Controls.Add(this.PBarAudio);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ProgressBar PBarAudio;
+    }
+}
+
---
+++ VoiceToText/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
+using System.Globalization;
+using Microsoft.Speech.Recognition.SrgsGrammar;
+
+namespace VoiceToText
+{
+    public partial class Form1 : Form
+    {
+        static CultureInfo ci = new CultureInfo("pt-BR");
+
+        static SpeechRecognitionEngine reconhecedor;
+
+        SpeechSynthesizer resposta = new SpeechSynthesizer();
+
+        public string[] listaPalavras = { "oi", "quem é você", "como você está" };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Init();
+            
+        }
+
+        public void Gramatica()
+        {
+            try
+            {
+                reconhecedor = new SpeechRecognitionEngine(ci);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            var gramatica = new Choices();
+            gramatica.Add(listaPalavras);
+
+            var gb = new GrammarBuilder();
+            gb.Append(gramatica);
+
+            try
+            {
+                var g = new Grammar(gb);
+
+                try
+                {
+                    reconhecedor.RequestRecognizerUpdate();
+                    reconhecedor.LoadGrammarAsync(g);
+
+                    reconhecedor.SpeechRecognized += Sre_Reconhecimento;
+                    reconhecedor.SetInputToDefaultAudioDevice();
+
+                    resposta.SetOutputToDefaultAudioDevice();
+                    reconhecedor.RecognizeAsync(RecognizeMode.Multiple);
+                    reconhecedor.AudioLevelUpdated += new EventHandler<AudioLevelUpdatedEventArgs>(audio_Level);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Erro ao criar reconhecedor" + e.Message);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Erro ao criar a gramática" + ex.Message);
+            }
+        }
+
+        private void Reconhecedor_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init()
+        {
+            resposta.Volume = 100;
+            resposta.Rate = 2;
+            Gramatica();
+        }
+
+        void Sre_Reconhecimento(object sender, SpeechRecognizedEventArgs e)
+        {
+            string frase = e.Result.Text;
+
+            richTextBox1.Text += frase + " ";
+            switch (frase)
+            {
+                case "oi":
+                    resposta.SpeakAsync("Olá, em que posso ajuda-lo!");
+                    break;
+                case "quem é você":
+                    resposta.SpeakAsync("sou sua namorada virtual!");
+                    break;
+                case "como você está":
+                    resposta.SpeakAsync("nossa você é muito fofo, estou bem e você");
+                    break;
+            }
+                
+            
+            //else
+            //{
+            //    resposta.SpeakAsync("Estou esperando você falar algo.");
+            //}
+        }
+
+        void audio_Level(object sender, AudioLevelUpdatedEventArgs e)
+        {           
+            PBarAudio.Maximum = 100;
+            PBarAudio.Value = e.AudioLevel;
+        }
+    }
+}
